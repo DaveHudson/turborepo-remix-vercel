df2298cf5bf411a00134b4119bd6a979896b930c
Dave Hudson <dave@applification.net>
2022-01-26 14:25:28 +0000

    Green button change

--- packages/ui/src/Button.tsx
+++ packages/ui/src/Button.tsx
@@ -3,7 +3,7 @@ import * as React from "react";
 export const Button = () => {
   return (
     <>
-      <button className="text-cyan-400">Boop UI?</button>
+      <button className="text-green-400">Boop UI?</button>
       <p className="text-pink-800">Some other text</p>
     </>
   );
